fb6673504a0783e64be2d66173fd406a17b266d7
Aleksandr Lichko <sanek05077@gmail.com>
2021-11-20 13:23:16 +0200

    01-basics: added calculator

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,10 +1,10 @@
- import { createApp } from './vendor/vue.esm-browser.js';
+ import { createApp } from './vendor/vue.esm-browser.js'
 
 const vm = createApp({
   data() {
     return {
       count: 0
-    };
+    }
   }
 
 }).mount('#app');
--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js'
 
-// Создайте Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      firstNumber: 10,
+      secondNumber: 5,
+      operator: 'sum'
+    }
+  },
+  computed: {
+    result () {
+      switch(this.operator) {
+        case 'sum':
+          return parseInt(this.firstNumber) + parseInt(this.secondNumber)
+        case 'subtract':
+          return parseInt(this.firstNumber) - parseInt(this.secondNumber)
+        case 'multiply':
+          return parseInt(this.firstNumber) * parseInt(this.secondNumber)
+        case 'divide':
+          return parseInt(this.firstNumber) / parseInt(this.secondNumber)
+        default:
+          return 'Incorrect operator'
+      }
+    }
+  }
+
+}).mount('#app')

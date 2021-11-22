14bcdcb968cf8fd7838ae14a851b9211e46d0d1d
Aleksandr Lichko <aleksandr.lichko@hyuna.bb>
2021-11-22 19:03:31 +0200

    01-basics: added meetup title

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,46 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
+const App = {
+  data() {
+    return {
+      meetupName: null,
+    };
+  },
+  methods: {
+    fetchMeetupById(meetupId) {
+      return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
       });
-    }
-  });
-}
+    },
+  },
+  watch: {
+    meetupName: {
+      deep: true,
+      immediate: true,
+      handler(meetupId) {
+        if (meetupId >= 1) {
+          const results = this.fetchMeetupById(meetupId).then(data => {
+            console.log(data.title)
 
-// Требуется создать Vue приложение
+          });
+        }
+      },
+    },
+  },
+};
+
+const app = createApp(App);
+
+const vm = app.mount('#app');
+
+window.vm = vm;
+
+

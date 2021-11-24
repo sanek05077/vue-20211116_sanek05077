569307328a70d8b15fff74603a94d36dee3e5d0b
Aleksandr Lichko <aleksandr.lichko@hyuna.bb>
2021-11-24 15:35:40 +0200

    02-components: added counter-button-component

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -2,11 +2,18 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'CounterButton',
-
-  // Компонент должен иметь входной параметр
-
-  // Шаблон лучше держать максимально простым, а логику выносить в методы
-
-  // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  props: {
+    count: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  methods: {
+    increaseCounter(counter) {
+      counter++;
+      this.$emit('update:count', counter);
+    },
+  },
+  template: `<button type="button" @click="increaseCounter(count)">{{ count }}</button>`,
 });

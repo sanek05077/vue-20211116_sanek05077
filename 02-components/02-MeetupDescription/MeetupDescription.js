a3a9a0baa407b352820331cb7c86f734fde58b90
Aleksandr Lichko <aleksandr.lichko@hyuna.bb>
2021-11-25 17:08:09 +0200

    02-components: added meetup-agenda

--- 02-components/02-MeetupDescription/MeetupDescription.js
+++ 02-components/02-MeetupDescription/MeetupDescription.js
@@ -1,12 +1,11 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
 
-
 export default defineComponent({
   name: 'MeetupDescription',
   props: {
     description: {
-      type: String
-    }
+      type: String,
+    },
   },
 
   template: `<p class="meetup-description">{{ description }}</p>`,
--- 02-components/05-MeetupAgenda/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -1,13 +1,23 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
+  components: {
+    MeetupAgendaItem,
+  },
+
+  props: {
+    agenda: {
+      type: Array,
+      required: true,
+    },
+  },
 
   template: `
     <ul class="agenda">
       <li class="agenda__item">
-        <!-- meetup agenda item -->
+        <meetup-agenda-item v-for="agendaItem in agenda" :agendaItem="agendaItem" :key="agendaItem.id" />
       </li>
     </ul>`,
 });

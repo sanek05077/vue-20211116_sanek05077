da1e15884f1f663ec0c205e33457ea591b096a42
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-12-15 18:30:06 +0300

    Added task GeneratedForm

---
+++ 07-forms/04-GeneratedForm/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="sample wrapper bg-grey page">
+    <div class="container">
+      <pre><code>{{ agendaItem }}</code></pre>
+      <meetup-agenda-item-form v-model:agenda-item="agendaItem" @remove="handleRemove" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MeetupAgendaItemForm from './components/MeetupAgendaItemForm';
+
+export default {
+  name: 'App',
+
+  components: { MeetupAgendaItemForm },
+
+  data() {
+    return {
+      agendaItem: {
+        id: 0,
+        startsAt: '00:00',
+        endsAt: '00:00',
+        type: 'other',
+        title: null,
+        description: null,
+        speaker: null,
+        language: null,
+      },
+    };
+  },
+
+  methods: {
+    handleRemove() {
+      alert('removed');
+    },
+  },
+};
+</script>
+
+<style>
+@import '~@/assets/styles/_layout.css';
+@import '~@/assets/styles/_container.css';
+</style>

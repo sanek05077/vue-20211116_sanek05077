02a6f7e57f3acd1bb9375b24645224d678f146cb
Aleksandr Lichko <aleksandr.lichko@hyuna.bb>
2021-12-16 15:04:11 +0200

    07-forms: added CounterButton

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,34 @@
 <template>
-  <button></button>
+  <button @click="localCounter += 1">{{ localCounter }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+  emits: ['update:count'],
+  data() {
+    return {
+      localCounter: this.count,
+    };
+  },
+  watch: {
+    count: {
+      immediate: true,
+      handler(newValue) {
+       this.localCounter = newValue;
+      },
+    },
+    localCounter: {
+      handler(newValue) {
+        this.$emit('update:count', newValue);
+      },
+    },
+  },
 };
 </script>
